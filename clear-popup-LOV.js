37c85b202bcc2d4f1cc69ad61a754840ff4ad485
Md Sumon Islam <wpdev.sumon@gmail.com>
2024-07-15 17:55:47 +0600

    Update clear-popup-LOV.js

--- clear-popup-LOV.js
+++ clear-popup-LOV.js
@@ -1,4 +1,7 @@
-//To clear popup LOV search data chreate a dynamic action on page load event and select execute java script code and paste is there
+//To clear popup LOV search data chreate a dynamic action on page load event
+//and select execute java script code and paste is there
+//give this class name reset-filter in advance section to the item that you want to clear
+
 $(".reset-filter").change((e) => {
   $(
     `#PopupLov_${apex.env.APP_PAGE_ID}_${$(e.currentTarget).attr(
