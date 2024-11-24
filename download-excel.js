6e57da66633d9c2bbc867a8a801464ae555f68db
Md Sumon Islam <wpdev.sumon@gmail.com>
2024-11-24 16:52:57 +0600

    Create download-excel.js

---
+++ download-excel.js
@@ -0,0 +1,22 @@
+//Copy and paste bellow code at JavaScript Function and Global Variable Declaration area
+var tableToExcel = (function () {
+        // Define your style class template.
+        var style = "<style>.green { background-color: green; }</style>";
+        var uri = 'data:application/vnd.ms-excel;base64,'
+            , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' + style + '</head><body><table>{table}</table></body></html>'
+            , base64 = function (s) {
+                return window.btoa(unescape(encodeURIComponent(s)))
+            }
+            , format = function (s, c) {
+                return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; })
+            }
+        return function (table, name) {
+            if (!table.nodeType) table = document.getElementById(table)
+            var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
+            window.location.href = uri + base64(format(template, ctx))
+        }
+    })()
+
+//create a dynamic action and action will execute javascript code
+//copy and paste bellow code and replace PrintAreaID with your report id
+tableToExcel('PrintAreaID', 'Attendance Report');
